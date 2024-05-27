63212aa4c0fa313d0c660f5a995450a4e2ee2427
ptobechi <ptobechii@gmail.com>
2024-05-27 15:19:53 -0500

    arrow functions

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+export default getNeighborhoodsList = () => {
+	this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+	const self = this;
+
+	this.addNeighborhood = add(newNeighborhood) => {
+		self.sanFranciscoNeighborhoods.push(newNeighborhood);
+		return self.sanFranciscoNeighborhoods;
+	};
+}
+
