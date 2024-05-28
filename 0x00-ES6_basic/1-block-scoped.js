974c17957a2e63e8e0d121314771a24342a2e4a4
ptobechi <ptobechii@gmail.com>
2024-05-28 05:38:09 -0500

    correction

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-	let task = false;
-	let task2 = true;
+  const task = false;
+  const task2 = true;
 
-	if (trueOrFalse) {
-		let task = true;
-		let task2 = false;
-	}
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
 
-	return [task, task2];
+  return [task, task2];
 }
 
