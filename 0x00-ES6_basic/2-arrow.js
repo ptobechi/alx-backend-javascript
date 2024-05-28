761fc590c6280eaeb3cfaf438daecf5b4f5f2b34
ptobechi <ptobechii@gmail.com>
2024-05-28 05:45:08 -0500

    corrections

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,9 @@
 export default function getNeighborhoodsList() {
-	this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-	const self = this;
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  const self = this;
 
-	this.addNeighborhood = (newNeighborhood) => {
-		self.sanFranciscoNeighborhoods.push(newNeighborhood);
-		return self.sanFranciscoNeighborhoods;
-	};
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
 }
-
