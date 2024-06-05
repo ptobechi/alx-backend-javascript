0c1060d0c4cdf66e97dae3da496ae59dc7389704
ptobechi <ptobechii@gmail.com>
2024-06-05 09:00:44 -0500

    correction

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,23 @@
-export function getListStudents() {
-    return [
-        {
-            id: 1,
-            firstName: 'Guillaume',
-            location: 'San Francisco'
-        },
-        {
-            id: 2,
-            firstName: 'James',
-            location: 'Columbia'
-        },
-        {
-            id: 5,
-            firstName: 'Serena',
-            location: 'San Francisco'
-        }
-    ];
-}
+// students.js
+
+const getListStudents = () => {
+	return [
+		{
+			id: 1,
+			firstName: "Guillaume",
+			location: "San Francisco",
+		},
+		{
+			id: 2,
+			firstName: "James",
+			location: "Columbia",
+		},
+		{
+			id: 5,
+			firstName: "Serena",
+			location: "San Francisco",
+		},
+	];
+};
+
+module.exports = { getListStudents };
