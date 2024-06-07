6b0262f39826e951182df870fafcccc419d178a7
ptobechi <ptobechii@gmail.com>
2024-06-07 16:22:44 -0500

    complete corrections

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+function cleanSet(set, startString) {
+	if (typeof startString !== 'string' || startString.length === 0) {
+		return '';
+	}
+
+	const result = [];
+
+	for (const value of set) {
+		if (value.startsWith(startString)) {
+			result.push(value.slice(startString.length));
+		}
+	}
+
+	return result.join('-');
+}
+
+module.exports = cleanSet;
