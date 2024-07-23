f89604cd9f77a707189a038144822520b630c08c
ghstnet <jamespeteer6363@gmail.com>
2024-07-23 17:19:11 +0100

    cc

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,9 @@
 /**
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
 const displayMessage = (msg) => {
-    console.log(msg);
+  console.log(msg);
 };
 
 module.exports = displayMessage;
